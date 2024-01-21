58403c84cdf0df11b890a352ace8148a826df775
MatiasFochi <matiasfochi.dev@gmail.com>
2024-01-21 17:40:44 -0300

    Actualizacion 21/01/2024

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Formulario from './componentes/Formulario.js';
 import Carrousel from './componentes/carousel.js';
 import Descripcion from './componentes/Descripcion.js';
 
-//<img className='FondoLogos' src={FONDO} alt='Fondo de logos' />
-
 function App() {
 
   return (
@@ -35,22 +33,3 @@ function App() {
 }
 
 export default App;
-
-/*
-      <AppContenedor
-        vinculo= 'https://startling-marzipan-de3cc9.netlify.app' 
-        nombre= 'Calculadora' 
-      />
-      <AppContenedor 
-        vinculo='https://soft-liger-d3063a.netlify.app'
-        nombre='Administrador de Tareas'
-      />
-      <AppContenedor 
-        vinculo= 'https://frabjous-taffy-5e61b4.netlify.app' 
-        nombre= 'Testimonios' 
-      />
-      <AppContenedor 
-        vinculo='https://cerulean-tartufo-5802ec.netlify.app'
-        nombre='Contador de clicks'
-      />
-*/
--- src/componentes/Cabecera.js
+++ src/componentes/Cabecera.js
@@ -4,34 +4,40 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import "@fontsource/montserrat";
 import "@fontsource/montserrat/400.css";
 import "@fontsource/montserrat/400-italic.css";
+import LogoMF from '../logos/LogoPersonalBlanco2.png';
 
 function Cabecera(props) {
   return(
-    <div className='container align-items-center justify-content-center espaciado espaciado-lateral sombreado row row-cols-2 contenedor-principal-cabecera'>
-      <div className='row col col-8 justify-content-center'>
-        <div className="card-body col espaciado">
-            <p className="card-text text-center  fw-bold cabecera-titulo-principal">Matias Fochi</p>
-            <p className="card-text text-center  fw-semibold cabecera-titulo-secundario">Desarrollador Front-End</p>
+    <div className='row row-cols-3 justify-content-around align-items-center espaciado contenedor-principal-cabecera'>  {/* Div principal (3 columnas, 1 fila) */}
+      <div className="col col-3 logo-cabecera-contenedor" >                     {/* 1er columna */}
+        <img src={LogoMF} className="logo-cabecera" alt="Logo MF" />
+      </div>
+      <div className='col-6 row row-cols-1 justify-content-center align-items-between contenedor-texto-cabecera'>            {/* 2da columna (3 filas) */}
+        <div className='col-12 contenedor-titulo'>                                                                             {/* 1ra fila */}
+          <h2 className='text-center fw-bold cabecera-titulo-principal'>Matias Fochi</h2>
         </div>
-        <div className='row col justify-content-center espaciado-lateral'>
-          <div className='col col-sm-auto icono-contenedor'>
+        <div className='col-12 contenedor-subtitulo'>                                                                             {/* 1ra fila */}
+          <h4 className='text-center  fw-semibold cabecera-titulo-secundario'>Desarrollador Front-End</h4>  {/* 2da fila */}
+        </div>
+        <div className='col-12 row row-cols-3 justify-content-evenly iconos-contenedor-cabecera'>
+          <div className='col-2 icono-contenedor'>
             <a href='https://github.com/MatiasFochi' target='_blank' rel="noreferrer">
               <i className='bi bi-github icono'></i>  
             </a>  
           </div>
-          <div className='col col-sm-auto icono-contenedor'>
+          <div className='col-2 icono-contenedor'>
             <a href='https://www.linkedin.com/in/matias-fochi/' target='_blank' rel="noreferrer">
               <i className='bi bi-linkedin icono'></i>  
             </a>
           </div>
-          <div className='col col-sm-auto icono-contenedor'>
+          <div className='col-2 icono-contenedor'>
             <a href='https://www.instagram.com/matias_fochi?igsh=bXR6ejJhZ3NuNm84' target='_blank' rel="noreferrer">
               <i className='bi bi-instagram icono'></i> 
             </a>
           </div>
         </div>
-      </div>      
-      <div className="col col-4 perfil espaciado espaciado-lateral" >
+      </div>
+      <div className="col col-3 perfil-cabecera-contenedor" >                   {/* 3er columna */}
         <img src={FotoPerfil} className="perfil" alt="Foto de perfil" />
       </div>
     </div>
@@ -40,40 +46,35 @@ function Cabecera(props) {
 
 export default Cabecera;
 
-//Columna Titulo
-
 /*
 
-*/
-
-//Columna Imagen
-
-/*
-      
-*/
-
-//Columna links-logos
-
-/*
-
-
-*/
+1er columna
 
 
 
+2da columna
 
-/*
-  <div className='container text-center row'>
-      <div className='uno col-8'>
-          <h1 className='nombre' >Matias Fochi</h1>
-          <p className='subtitulo'>Desarrollador Front-End</p> 
+        <div className='col col-sm-auto icono-contenedor'>
+          <a href='https://github.com/MatiasFochi' target='_blank' rel="noreferrer">
+            <i className='bi bi-github icono'></i>  
+          </a>  
+        </div>
+        <div className='col col-sm-auto icono-contenedor'>
+          <a href='https://www.linkedin.com/in/matias-fochi/' target='_blank' rel="noreferrer">
+            <i className='bi bi-linkedin icono'></i>  
+          </a>
+        </div>
+        <div className='col col-sm-auto icono-contenedor'>
+          <a href='https://www.instagram.com/matias_fochi?igsh=bXR6ejJhZ3NuNm84' target='_blank' rel="noreferrer">
+            <i className='bi bi-instagram icono'></i> 
+          </a>
+        </div>
+
+
+3ra columna
+
+      <div className="col col-4 perfil espaciado espaciado-lateral" >
+        <img src={FotoPerfil} className="perfil" alt="Foto de perfil" />
       </div>
-      <div className='uno col'>
-          <img
-          className='foto-perfil' 
-          src = {FotoPerfil} 
-          alt = 'Foto de perfil'
-          width={120}/>
-      </div>
-  </div>
+
 */
